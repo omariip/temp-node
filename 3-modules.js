fd0bf0af97765c2d716c9cb3aecf94dc650249a0
omariip <aboushaar.omar@gmail.com>
2022-05-08 05:08:46 +0300

    first commit

---
+++ 3-modules.js
@@ -0,0 +1,14 @@
+// CommonJS, every file is module (by default)
+// Modules - Encapsulated code (only share minimum)
+
+
+//not working anymore
+const names = require('./4-names');
+const sayHi = require('./5-hiFunc');
+const data = require('./6-alternative');
+require('./7-mind-grenade');
+
+// console.log(data);
+// sayHi('omar');
+// sayHi(names.john);
+// sayHi(names.peter);
---
+++ 4-names.js
@@ -0,0 +1,8 @@
+// local var
+const secret = 'Super Secret'
+
+// shared var
+const john = 'john';
+const peter = 'peter';
+
+module.exports = {john, peter}
